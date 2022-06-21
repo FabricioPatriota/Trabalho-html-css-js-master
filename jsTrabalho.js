5dd4e8609e69aa3795b0e3dee0d23b753108bae1
Fabricio Patriota <fpatriota60@gmail.com>
2022-06-21 20:31:54 -0300

    primeiro comentario

---
+++ jsTrabalho.js
@@ -0,0 +1,114 @@
+var jsonTrabalho = [
+  {"id": 1, "nacao": "Nação: Orc Snaga", "casa": "Casa: Cirith Ungol", "descricao": "Descricao: Snaga era o segundo no comando do Bando de Grishnakh. Snaga e Grishnakh encontraram o bando de Ugluk e mandaram Ugluk levar os Hobbits para o Grande Rio onde um Nazgul esperava por eles mas Ugluk não obedeceu e depois ele junto com todos os uruks e orcs correram a toda velocidade até a floresta quando Mauhur um Uruk Hai sentiu o cheiro de carne humana. Na Floresta Fangorn os orcs e uruks discutiram pelos hobbits pois Grishnakh e Snaga queriam devoralos enquanto Ugluk e os uruks queriam leva los até Isengard. Snaga encontrou seu fim quando tentou agarrar Pippin sem sucesso foi decapitado por Ugluk e canibalizado pelos Uruks. Nasceu no Ano 2956 e morreu no ano 3019.", "personagens":["Bilbo Bolseiro", "Samwise Gamgee", "Frodo Bolseiro", "Peregrin Tûk", "Rosinha Villa"], "imagem":"http://tolkiengateway.net/w/images/thumb/a/a1/Randy_Gallegos_and_Quinton_Hoover_-_Gallegos.jpg/250px-Randy_Gallegos_and_Quinton_Hoover_-_Gallegos.jpg"},
+  {"id": 2, "nacao": "Nação: Uruk-Hai", "casa": "Casa: Portão Negro", "descricao": "Descricao: Foram uma linhagem de orcs criados por Saruman na Terceira Era, em Mordor, segundo Gandalf seriam, supostamente, um cruzamento entre humanos e goblins, como especulou Barbárvore. Os Uruk-hai apareceram pela primeira vez em 2475 quando invadiram Ithilien e tomaram Osgiliath. Ao tempo da Guerra do Anel os uruks eram utilizados por Sauron e Saruman como soldados em todas as frentes. Gorbag, Shagrat, Uglúk e talvez Azog e Bolg eram uruks. Os Uruk-hai eram mais usados como soldados do que as outras linhagens porque, ao contrário dos outros orcs, eram quase tão altos quanto os homens, tinham pernas longas e retas e não se enfraqueciam à luz do Sol. Eram negros e possuíam olhos cerrados. Pelas suas vantagens evidentes, os uruks consideravam-se superiores aos demais orcs. Eles foram Elfos outrora, mutilados e torturados pelos Senhores das Trevas. Escravos da vontade de Sauron. Os Uruk-Hai eram extremamente cruéis e selvagens. São incansáveis, o ódio era o seu único sentimento, não tinham quaisquer emoções. Não receavam a morte e só pensavam em matar.", "personagens":["Bilbo Bolseiro", "Samwise Gamgee", "Frodo Bolseiro", "Peregrin Tûk", "Rosinha Villa"], "imagem":"https://assets.mycast.io/actor_images/actor-uruk-hai-280118_large.jpg?1633357490"},
+  {"id": 3, "nacao": "Nação: Cavaleiro de Rohan", "casa": "Casa: Abismo de Helm", "descricao": "Descricao: Enquanto os cavaleiros de Rohan lutavam em frente aos portões de Minas Tirith, na grande batalha da terceira era, os Ents novamente liderados por Barbárvore atacaram um grande exército de Mordor que atravessou o Anduin para atacar Rohan.", "personagens":["Bilbo Bolseiro", "Samwise Gamgee", "Frodo Bolseiro", "Peregrin Tûk", "Rosinha Villa"], "imagem":"https://i.pinimg.com/originals/f2/87/21/f287217c96ee2570dbbad6363bcf6f75.jpg"},
+  {"id": 4, "nacao": "Nação: Guerreiro de Gondor", "casa": "Casa: Minas Tirith", "descricao": "Descricao: Gondor é um reino fictício nos escritos de JRR Tolkien , descrito como o maior reino dos Homens no oeste da Terra-média no final da Terceira Era . O terceiro volume de O Senhor dos Anéis , O Retorno do Rei , está amplamente preocupado com os eventos em Gondor durante a Guerra do Anel e com a restauração do reino depois. A história do reino é descrita nos apêndices do livro.Segundo a narrativa, Gondor foi fundada pelos irmãos Isildur e Anárion, exilados do caído reino insular de Númenor . Junto com Arnor no norte, Gondor, o reino do Sul, serviu como último reduto dos Homens do Oeste . Após um período inicial de crescimento, Gondor declinou gradualmente à medida que a Terceira Era avançava,sendo continuamente enfraquecida por conflitos internos e conflitos com os aliados do Lorde das Trevas Sauron . Na época da Guerra do Anel, o trono de Gondor está vazio, embora seus principados e feudos ainda prestem deferência ao rei ausente, mostrando sua lealdade aos Regentes de Gondor. A ascendência do reino foi restaurada apenas com a derrota final de Sauron e a coroação deAragorn como rei.", "personagens":["Bilbo Bolseiro", "Samwise Gamgee", "Frodo Bolseiro", "Peregrin Tûk", "Rosinha Villa"], "imagem":"https://pm1.narvii.com/6855/dba7554cec65beca95100a7a732f250bc896fd12v2_hq.jpg"},
+  {"id": 5, "nacao": "Nação: Hobbit", "casa": "Casa: O Condado", "descricao": "Descricao: Os hobbits pés-peludos são o tipo mais comum de hobbit. São morenos, preferem planícies e regiões banhadas por rios. Os cascalvas tem a pele e o cabelo mais claros, são mais altos e mais esguios que os outros tipos de hobbit, preferem bosques e florestas para morar além de serem descendentes de elfos. Outros hobbits dizem que estes têm 'sangue de fada', por conta de sua ascendência. Os grados são tipos de hobbit mais robustos e frequentemente cultivam barbas. São mais próximos aos anões, e Sméagol costumava ser um cascalva antes de ser corrompido pelo anel", "personagens":["Bilbo Bolseiro", "Samwise Gamgee", "Frodo Bolseiro", "Peregrin Tûk", "Rosinha Villa"], "imagem":"https://static3.tcdn.com.br/img/img_prod/460977/action_figure_bilbo_baggins_the_hobbit_escala_1_4_neca_apenas_venda_online_53069_1_20201211173118.jpg"},
+  {"id": 6, "nacao": "Nação: Elfo da Floresta", "casa": "Casa: Floresta das Trevas", "descricao": "Descricao: Elfos da Floresta são um povo mágico e selvagem, de ferocidade e destreza sobrenatural, que domam e se tornam parte das florestas que habitam. Assim como todos os elfos, eles vivem em lugares de beleza etérea, mas eles tem preferências pelas florestas e selvas de Iccarion, adotando lugares hostis como suas casas e expandindo seus conhecimentos naturais e mágicos.Dentre todas as raças élficas, os Elfos da Floresta são as mais ligadas a natureza do plano material, muitas vezes se considerando parte da própria, não diferentes das feras e animais que as habitam.", "personagens":["Bilbo Bolseiro", "Samwise Gamgee", "Frodo Bolseiro", "Peregrin Tûk", "Rosinha Villa"], "imagem":"https://static.wikia.nocookie.net/iccarion/images/8/84/Elfo_do_olho_da_mata.jpg/revision/latest/scale-to-width-down/241?cb=20210219040421&path-prefix=pt-br"},
+  {"id": 7, "nacao": "Nação: Elfo de Lothlórien", "casa": "Casa: Caras Galadhon", "descricao": "Descricao: No início da Primeira Era alguns dos Eldar deixaram a Grande Marcha e se estabeleceram nas terras a leste das Montanhas Sombrias. Estes elfos ficaram conhecidos como os Nandor e mais tarde os Elfos Silvan. Em 1.200 da Segunda Era, Galadriel tinha feito contato com um reino Nandorin existente, Lindórinand, na área que mais tarde seria conhecida como Lothlórien, e ali plantou as árvores douradas mallorn que Gil-galad tinha recebido como um presente de Tar-Aldarion. A cultura e os conhecimentos dos Elfos Silvan foi enriquecida consideravelmente com a chegada de Elfos Sindarin do oeste das montanhas e até mesmo a linguagem Sindarin foi gradualmente substituída pelo Sindarin. Entre estes que chegaram estava Amdír, que se tornou seu primeiro senhor, assim como Galadriel e Celeborn, que também cruzaram as montanhas e o Anduin para se juntar aos Nandor do sul após a destruição de Eregion durante a Guerra dos Elfos e Sauron. Em última análise, Amdír liderou um exército para fora da floresta como parte da Última Aliança Entre Elfos e Homens, assim como Oropher, outro senhor Sindarin, levou os Elfos Silvan do norte à mesma vitória sobre Sauron, por isso pode-se supor que tanto reinos da floresta do norte e do sul tinham sido fundadas até então. Com o retorno gradual da influência maligna de Sauron, a leste da floresta e do Anduin, os Elfos Silvan do norte liderados por Thranduil, filho de Oropher (e pai de Legolas), mudaram-se ainda mais ao norte para escapar dele, e os do sul voltaram ao oeste através do Anduin, embora sem o seu último senhor sindarin Amroth, filho de Amdír, que partiu para Edhellond após sua amante Nimrodel fugir para lá. Após a saída de Galadriel para Valinor no início da Quarta Era, os elfos de Lothlórien passaram a ser governados unicamente por Celeborn, que levou os elfos para além do Anduin para fundar um reino novo e maior, Lórien do Leste, centrado em Amon Lanc. Até o momento da morte da Rainha Arwen, neta de Celeborn e Galadriel, a própria Lothlórien estava deserta.", "personagens":["Bilbo Bolseiro", "Samwise Gamgee", "Frodo Bolseiro", "Peregrin Tûk", "Rosinha Villa"], "imagem":"https://http2.mlstatic.com/D_NQ_NP_872333-MLB31500482367_072019-W.jpg"},
+  {"id": 8, "nacao": "Nação: Anão", "casa": "Casa: Colinas de Ferro", "descricao": "Descricao: Anões são uma raça de seres que habita Arda, a Terra pré-histórica fictícia que inclui o continente da Terra Média, nas obras fictícias de J. R. R. Tolkien. São geralmente ferreiros ou mineradores, inigualáveis até mesmo frente aos elfos em algumas de suas artes.", "personagens":["Bilbo Bolseiro", "Samwise Gamgee", "Frodo Bolseiro", "Peregrin Tûk", "Rosinha Villa"], "imagem":"https://2.bp.blogspot.com/-Ij80gfjhx-4/Wsah7xS2bxI/AAAAAAAAGrU/2Ttyih4HF6kYJCS6RUxWrk5tSEM1l3ImACLcBGAs/s1600/Gimli5.jpg"},
+]
+function filtroSnaga(valor) {
+  return valor.id == "1";
+}
+function filtroUruk(valor) {
+  return valor.id == "2";
+}
+function filtroRohan(valor) { 
+  return valor.id == "3";
+}
+function filtroGondor(valor) {
+  return valor.id == "4";
+}
+function filtrohobbit(valor) {
+  return valor.id == "5";
+}
+function filtroFloresta(valor) {
+  return valor.id == "6";
+}
+function filtroLothlorien(valor) {
+  return valor.id == "7";
+}
+function filtroAnao(valor) {
+  return valor.id == "8";
+}
+function escreveSnaga(snaga){
+  console.log("clicou em 'Orc Snaga'");
+  let personagemFiltrado = jsonTrabalho.filter(filtroSnaga);
+  escreveHtml(personagemFiltrado);
+}
+function escreveUruk(uruk){
+  console.log("clicou em 'Uruk-Hai'");
+  let personagemFiltrado = jsonTrabalho.filter(filtroUruk);
+  escreveHtml(personagemFiltrado);
+}
+function escreveRohan(rohan){
+  console.log("clicou em 'Cavaleiro de Rohan'");
+  let personagemFiltrado = jsonTrabalho.filter(filtroRohan);
+  escreveHtml(personagemFiltrado);
+}
+function escreveGondor(gondor){
+  console.log("clicou em 'Guerreiro de Gondor'");
+  let personagemFiltrado = jsonTrabalho.filter(filtroGondor);
+  escreveHtml(personagemFiltrado);
+}
+function escrevehobbit(hobbit){
+  console.log("clicou em 'hobbit'");
+  let personagemFiltrado = jsonTrabalho.filter(filtrohobbit);
+  escreveHtml(personagemFiltrado);
+}
+function escreveFloresta(floresta){
+  console.log("clicou em 'Elfo da Floresta'");
+  let personagemFiltrado = jsonTrabalho.filter(filtroFloresta);
+  escreveHtml(personagemFiltrado);
+}
+function escreveLothlorien(lothlorien){
+  console.log("clicou em 'Elfo de Lothlórien'");
+  let personagemFiltrado = jsonTrabalho.filter(filtroLothlorien);
+  escreveHtml(personagemFiltrado);
+}
+function escreveAnao(anao){
+  console.log("clicou em 'Anão'");
+  let personagemFiltrado = jsonTrabalho.filter(filtroAnao);
+  escreveHtml(personagemFiltrado);
+}
+
+function escreveHtml(exibir_descricao) {
+  exibir_descricao = exibir_descricao.sort();
+    var meuNovoConteudo = "<div>";
+    for (let i = 0; i < exibir_descricao.length; i++) {
+      meuNovoConteudo += "<li>" + exibir_descricao[i].nacao + "</li>";
+      meuNovoConteudo += "<li>" + exibir_descricao[i].casa + "</li>";
+      meuNovoConteudo += "<li>" + exibir_descricao[i].descricao + "</li>";
+      meuNovoConteudo += "<br>"+"</br>"
+      meuNovoConteudo += "<li>" + '<img src="'+exibir_descricao[i].imagem+'"></img>' + "</li>";
+    }
+    meuNovoConteudo += "</div>";
+  var elemento = document.getElementById('jsonTrabalho');
+  elemento.innerHTML = meuNovoConteudo;
+}
+
+//Criação de personagens
+
+var jsonClone = [{"id": "1"},]
+
+function filtroDuplica(valor) {
+  return valor.id === "1";
+}
+function adicionarPersonagem(addPerson){
+  console.log("clicou em 'Adicionar Personagem'");
+  let personagemFiltrado = jsonClone.filter(filtroDuplica);
+  duplicaHtml(personagemFiltrado);
+}
+
+function duplicaHtml(duplicar_html) {
+  duplicar_html = duplicar_html.sort();
+    var duplicarel = "<div>";
+    for (let i = 0; i < duplicar_html.length; i++) {
+      let cloneform = document.querySelector("#crperson-container").cloneNode(true)
+      document.querySelector("#crperson-container").after(cloneform)
+    }
+    duplicarel += "</div>";
+  var elemento = document.getElementById('jsonTrabalho');
+  elemento.innerHTML = duplicarel;
+}
